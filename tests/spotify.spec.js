02ceb2a9d7eff107786fc835c6f88eed6d9d9b15
Daniel Silva <dani3l.dxp@gmail.com>
2020-05-26 20:29:31 -0300

    feat: criando os testes da lib spotify

---
+++ tests/spotify.spec.js
@@ -0,0 +1,17 @@
+'use strict';
+import { expect } from 'chai';
+import spotify from '../src/spotify';
+
+describe('Spotify', () => {
+	it('should be an object', () => {
+		expect(spotify).to.be.an('object');
+	});
+
+	it('should have search methods', () => {
+		expect(spotify.search).to.exist;
+    });
+    
+    it('should have album methods', () => {
+		expect(spotify.album).to.exist;
+	});
+});
