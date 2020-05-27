f6974c6060c934da06625124ca4eeb4b632b1302
Daniel Silva <dani3l.dxp@gmail.com>
2020-05-27 15:48:21 -0300

    feat: renderizando o component AlbumList no html

--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
-console.log('TESTE')
+'use strict';
+import spotify from './spotify';
+import renderAlbum from './albumList';
+
+const albums = spotify.search.albums('Zé Neto e Cristiano');
+const albumList = document.querySelector('#album-list')
+albums.then((data) => renderAlbum(data.albums.items, albumList))
