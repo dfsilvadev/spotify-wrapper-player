a712240e6ea96f47ff6c405d2c4c27347fbc29b4
Daniel Silva <dani3l.dxp@gmail.com>
2020-05-27 18:57:22 -0300

    feat: refatorando código

--- src/main.js
+++ src/main.js
@@ -3,5 +3,5 @@ import spotify from './spotify';
 import renderAlbum from './albumList';
 
 const albums = spotify.search.albums('Zé Neto e Cristiano');
-const albumList = document.querySelector('#album-list')
-albums.then((data) => renderAlbum(data.albums.items, albumList))
+const albumList = document.querySelector('#album-list');
+albums.then((data) => renderAlbum(data.albums.items, albumList));
--- tests/albumList.spec.js
+++ tests/albumList.spec.js
@@ -3,83 +3,101 @@ import { expect } from 'chai';
 import renderAlbums from '../src/albumList';
 
 describe('AlbumList', () => {
-  it('should exist', () => {
-    expect(renderAlbums).to.exist;
-  });
+	it('should exist', () => {
+		expect(renderAlbums).to.exist;
+	});
 
-  const data = [
-    {
-      "album_type" : "album",
-      "artists" : [ {
-        "name" : "Incubus",
-      } ],
-      "id" : "6peEdPVO73WtgGah5sEhX4",
-      "images" : [ {
-        "height" : 640,
-        "url" : "https://i.scdn.co/image/59a536f0bf0ddaa427e4c732a061c33fe7578757",
-        "width" : 640
-      }, {
-        "height" : 300,
-        "url" : "https://i.scdn.co/image/9d6866c93e476bd8e7aa7771f9b68db119e076c6",
-        "width" : 300
-      }, {
-        "height" : 64,
-        "url" : "https://i.scdn.co/image/3ad2701e3f6fe51404f3a4de7a5b2c7b745bad16",
-        "width" : 64
-      } ],
-      "name" : "The Essential Incubus",
-      "type" : "album",
-    }
-  ];
+	const data = [
+		{
+			album_type: 'album',
+			artists: [
+				{
+					name: 'Incubus'
+				}
+			],
+			id: '6peEdPVO73WtgGah5sEhX4',
+			images: [
+				{
+					height: 640,
+					url: 'https://i.scdn.co/image/59a536f0bf0ddaa427e4c732a061c33fe7578757',
+					width: 640
+				},
+				{
+					height: 300,
+					url: 'https://i.scdn.co/image/9d6866c93e476bd8e7aa7771f9b68db119e076c6',
+					width: 300
+				},
+				{
+					height: 64,
+					url: 'https://i.scdn.co/image/3ad2701e3f6fe51404f3a4de7a5b2c7b745bad16',
+					width: 64
+				}
+			],
+			name: 'The Essential Incubus',
+			type: 'album'
+		}
+	];
 
-  const dataTwo = [
-    {
-      "album_type" : "album",
-      "artists" : [ {
-        "name" : "Incubus",
-      } ],
-      "id" : "6peEdPVO73WtgGah5sEhX4",
-      "images" : [ {
-        "height" : 640,
-        "url" : "https://i.scdn.co/image/59a536f0bf0ddaa427e4c732a061c33fe7578757",
-        "width" : 640
-      }, {
-        "height" : 300,
-        "url" : "https://i.scdn.co/image/9d6866c93e476bd8e7aa7771f9b68db119e076c6",
-        "width" : 300
-      }, {
-        "height" : 64,
-        "url" : "https://i.scdn.co/image/3ad2701e3f6fe51404f3a4de7a5b2c7b745bad16",
-        "width" : 64
-      } ],
-      "name" : "The Essential Incubus",
-      "type" : "album",
-    },
-    {
-        "album_type" : "album",
-        "artists" : [ {
-          "name" : "Incubus",
-        } ],
-        "id" : "6peEdPVO73WtgGah5sEhX4",
-        "images" : [ {
-          "height" : 640,
-          "url" : "https://i.scdn.co/image/59a536f0bf0ddaa427e4c732a061c33fe7578757",
-          "width" : 640
-        }, {
-          "height" : 300,
-          "url" : "https://i.scdn.co/image/9d6866c93e476bd8e7aa7771f9b68db119e076c6",
-          "width" : 300
-        }, {
-          "height" : 64,
-          "url" : "https://i.scdn.co/image/3ad2701e3f6fe51404f3a4de7a5b2c7b745bad16",
-          "width" : 64
-        } ],
-        "name" : "The Essential Incubus",
-        "type" : "album",
-      }
-  ];
+	const dataTwo = [
+		{
+			album_type: 'album',
+			artists: [
+				{
+					name: 'Incubus'
+				}
+			],
+			id: '6peEdPVO73WtgGah5sEhX4',
+			images: [
+				{
+					height: 640,
+					url: 'https://i.scdn.co/image/59a536f0bf0ddaa427e4c732a061c33fe7578757',
+					width: 640
+				},
+				{
+					height: 300,
+					url: 'https://i.scdn.co/image/9d6866c93e476bd8e7aa7771f9b68db119e076c6',
+					width: 300
+				},
+				{
+					height: 64,
+					url: 'https://i.scdn.co/image/3ad2701e3f6fe51404f3a4de7a5b2c7b745bad16',
+					width: 64
+				}
+			],
+			name: 'The Essential Incubus',
+			type: 'album'
+		},
+		{
+			album_type: 'album',
+			artists: [
+				{
+					name: 'Incubus'
+				}
+			],
+			id: '6peEdPVO73WtgGah5sEhX4',
+			images: [
+				{
+					height: 640,
+					url: 'https://i.scdn.co/image/59a536f0bf0ddaa427e4c732a061c33fe7578757',
+					width: 640
+				},
+				{
+					height: 300,
+					url: 'https://i.scdn.co/image/9d6866c93e476bd8e7aa7771f9b68db119e076c6',
+					width: 300
+				},
+				{
+					height: 64,
+					url: 'https://i.scdn.co/image/3ad2701e3f6fe51404f3a4de7a5b2c7b745bad16',
+					width: 64
+				}
+			],
+			name: 'The Essential Incubus',
+			type: 'album'
+		}
+	];
 
-  const markup = `
+	const markup = `
     <div class="list-item">
       <img src="${data[0].images[2].url}" alt="${data[0].name}" class="list-image">
       <div class="list-description">
@@ -88,7 +106,7 @@ describe('AlbumList', () => {
       </div>
     </div>`;
 
-    const markupTwo = `
+	const markupTwo = `
     <div class="list-item">
       <img src="${data[0].images[2].url}" alt="${data[0].name}" class="list-image">
       <div class="list-description">
@@ -104,17 +122,17 @@ describe('AlbumList', () => {
       </div>
     </div>`;
 
-  it('should create and append the markup given a correct data', () => {
-    const element = document.createElement('div');
-    renderAlbums(data, element);
+	it('should create and append the markup given a correct data', () => {
+		const element = document.createElement('div');
+		renderAlbums(data, element);
 
-    expect(element.innerHTML).to.be.eql(markup);
-  });
+		expect(element.innerHTML).to.be.eql(markup);
+	});
 
-  it('should create and append when more than 1 album', () => {
-    const elementTwo = document.createElement('div');
-    renderAlbums(dataTwo, elementTwo);
+	it('should create and append when more than 1 album', () => {
+		const elementTwo = document.createElement('div');
+		renderAlbums(dataTwo, elementTwo);
 
-    expect(elementTwo.innerHTML).to.be.eql(markupTwo);
-  });
+		expect(elementTwo.innerHTML).to.be.eql(markupTwo);
+	});
 });
