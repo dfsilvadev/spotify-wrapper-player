bcadf11e89f1e5906a451993d3a4ac7b15c8e4af
Daniel Silva <dani3l.dxp@gmail.com>
2020-05-28 10:27:03 -0300

    feat: renderizando component no html

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 'use strict';
 import spotify from './spotify';
 import renderAlbum from './albumList';
+import renderAlbumInfo from './albumInfo';
 
 const albums = spotify.search.albums('Zé Neto e Cristiano');
 const albumList = document.querySelector('#album-list');
+
+const album = spotify.album.getAlbum('1Yg3QTRv9IGAWeJQb7q8bT');
+const albumInfo = document.querySelector('#album-info');
+
 albums.then((data) => renderAlbum(data.albums.items, albumList));
+album.then((data) => renderAlbumInfo(data, albumInfo));
