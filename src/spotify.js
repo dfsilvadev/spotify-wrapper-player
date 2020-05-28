d4687c660b777be3a8e8bda3841425c7aaa6e9ea
Daniel Silva <dani3l.dxp@gmail.com>
2020-05-28 10:24:54 -0300

    feat: alterando token para testes

--- src/spotify.js
+++ src/spotify.js
@@ -3,7 +3,7 @@ import SpotifyWrapper from 'dxp-spotify-api-wrapper';
 
 const spotify = new SpotifyWrapper({
 	apiURL: `https://api.spotify.com/v1`,
-	token: `BQDJMPb0qfwD-er-2Qzkvems26XSK4NotjtTCbGj1snhpbIVmPehE7KsOBo215XGfiIu8letvD9uBvX4fVtInchE2LBa0aEUIuuhq5gYJsvLjRXmj0cput9X9YxXZyjChiCaGmVZYIZqzzg`
+	token: `BQCEJ4P0cPSUC4F2WvaFo9fwllV2U_PUjiDgOeEU3jFLlA4eUxdMawKweO8Ut8FdtGwEvMfeiAj__HNCFnkwslX-HYndgPsUhUNLDTzvjasEv2tjO6bwiMhiBWnbfd8dEvR01jStTXkjE5s`
 });
 
 export default spotify;
