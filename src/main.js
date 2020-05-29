a3b16f2d5e74e1e0aa58375ee7a861b72944bbbb
Daniel Silva <dani3l.dxp@gmail.com>
2020-05-29 13:20:27 -0300

    feat: adicionando o método selectAlbumTrigger ao main

--- src/main.js
+++ src/main.js
@@ -1,16 +1,6 @@
 'use strict';
-import spotify from './spotify';
-import renderAlbumInfo from './albumInfo';
-import renderAlbumTracks from './albumTracks';
 import searchEnterTrigger from './searchTrigger';
+import selectAlbumTrigger from './selectAlbumTrigger';
 
 searchEnterTrigger();
-
-const albumInfo = document.querySelector('#album-info');
-const albumTracks = document.querySelector('#album-tracks');
-
-const album = spotify.album.getAlbum('1Yg3QTRv9IGAWeJQb7q8bT');
-
-album
-	.then((data) => renderAlbumInfo(data, albumInfo))
-	.then((data) => renderAlbumTracks(data.tracks.items, albumTracks));
+selectAlbumTrigger();
