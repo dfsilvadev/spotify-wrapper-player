e4c93882f9a498d21dc036562d5e20a807045df4
Daniel Silva <dani3l.dxp@gmail.com>
2020-05-29 15:04:10 -0300

    feat: adicionando o método playlistTrigger ao main

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 'use strict';
 import searchEnterTrigger from './searchTrigger';
 import selectAlbumTrigger from './selectAlbumTrigger';
+import playlistTrigger from './playlistTrigger';
 
 searchEnterTrigger();
 selectAlbumTrigger();
+playlistTrigger();
