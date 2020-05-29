b98dc893fefad9df16c7d726f3b2e434e66324a1
Daniel Silva <dani3l.dxp@gmail.com>
2020-05-29 13:20:39 -0300

    feat: refatorando o código

--- src/searchTrigger.js
+++ src/searchTrigger.js
@@ -6,9 +6,13 @@ const albumList = document.querySelector('#album-list');
 const searchInput = document.querySelector('#search-input');
 const searchForm = document.querySelector('#search-form');
 
+function makeRequest() {
+	spotify.search.albums(searchInput.value).then((data) => renderAlbum(data.albums.items, albumList));
+}
+
 export default function searchEnterTrigger() {
 	searchForm.addEventListener('submit', (e) => {
 		e.preventDefault();
-		spotify.search.albums(searchInput.value).then((data) => renderAlbum(data.albums.items, albumList));
+		makeRequest();
 	});
 }
