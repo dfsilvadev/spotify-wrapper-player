b28fecd76e50e6c00ee9b01a4fe1f18e8464af24
Daniel Silva <dani3l.dxp@gmail.com>
2020-05-29 12:03:53 -0300

    feat: criando o método converteToHumanTime

---
+++ src/converteToHumanTime.js
@@ -0,0 +1,9 @@
+'use strict';
+export default function convertToHumanTime(duration) {
+	let s = parseInt((duration / 1000) % 60, 10);
+	const m = parseInt((duration / (1000 * 60)) % 60, 10);
+
+	s = s < 10 ? `0${s}` : s;
+
+	return `${m}:${s}`;
+}
