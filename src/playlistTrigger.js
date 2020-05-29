46faf4defe3fcf489821e2b0b69697e7a33a7b47
Daniel Silva <dani3l.dxp@gmail.com>
2020-05-29 15:03:19 -0300

    feat: criando o método playlistTrigger

---
+++ src/playlistTrigger.js
@@ -0,0 +1,28 @@
+'use strict';
+const albumTracks = document.querySelector('#album-tracks');
+let audioObject = null;
+
+export default function playlistTrigger() {
+	albumTracks.addEventListener('click', (e) => {
+		const target = e.target.parentNode;
+
+		if (target.classList.contains('active')) {
+			audioObject.pause();
+		} else {
+			if (audioObject) {
+				audioObject.pause();
+			}
+			audioObject = new Audio(target.getAttribute('data-track-preview'));
+			audioObject.play();
+			target.classList.add('active');
+
+			audioObject.addEventListener('ended', () => {
+				target.classList.remove('active');
+			});
+
+			audioObject.addEventListener('pause', () => {
+				target.classList.remove('active');
+			});
+		}
+	});
+}
