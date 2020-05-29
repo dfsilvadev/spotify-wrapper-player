e1dda1370b8dd6f2ad47b71114073a50f5ad9814
Daniel Silva <dani3l.dxp@gmail.com>
2020-05-29 12:38:03 -0300

    feat: alternado o teken

--- src/spotify.js
+++ src/spotify.js
@@ -3,7 +3,7 @@ import SpotifyWrapper from 'dxp-spotify-api-wrapper';
 
 const spotify = new SpotifyWrapper({
 	apiURL: `https://api.spotify.com/v1`,
-	token: `BQBrF4Dt_WeQiLJDNhjr2HzTbHRgvTKZjFUHJbk8hJzO9QKwZ4NFpahsJF4dUAkw6Kh3p5y3wgQACOslYj4djBNrxWet2tTSUtKOYUcBawJEuQEO9bdqIEUxTOo1Ib4G7PGhRfnEo9aCQgI`
+	token: `BQCzqkpwKS01T3dU9OO8DtFpJDyaSFrAnA2GBdUP5lq4B-LfNUz2CkAMGDH-sI-1gxuNzjHy2YHPTHFiip6mbUPPmtDqRDmt5zr6Fd5gEgedflKRnH8lXwG2WMLMiMI12iLieWOLI0y53pI`
 });
 
 export default spotify;
