f070d4a10e0643b853ae91ddb9adcde7af7c88df
Daniel Silva <dani3l.dxp@gmail.com>
2020-05-29 10:41:25 -0300

    feat: criado testes para AlbumTracks

---
+++ tests/albumTracks.spec.js
@@ -0,0 +1,66 @@
+import 'jsdom-global/register';
+import { expect } from 'chai';
+import renderAlbumInfo from '../src/albumTracks';
+import renderAlbumTracks from '../src/albumTracks';
+
+describe('AlbumTracks', () => {
+	const data = [
+		{
+			preview_url:
+				'https://p.scdn.co/mp3-preview/ab3d501c5ffbf560e94094f76cd36d874a26e941?cid=8897482848704f2a8f8d7c79726a70d4',
+			track_number: 1,
+			name: 'Around The World',
+			duration_ms: 238733
+		}
+	];
+
+	const dataTwo = [
+		{
+			preview_url:
+				'https://p.scdn.co/mp3-preview/ab3d501c5ffbf560e94094f76cd36d874a26e941?cid=8897482848704f2a8f8d7c79726a70d4',
+			track_number: 1,
+			name: 'Around The World',
+			duration_ms: 238733
+		},
+		{
+			preview_url:
+				'https://p.scdn.co/mp3-preview/ab3d501c5ffbf560e94094f76cd36d874a26e941?cid=8897482848704f2a8f8d7c79726a70d4',
+			track_number: 1,
+			name: 'Around The World',
+			duration_ms: 238733
+		}
+	];
+
+	const markup = `
+        <div class="music" data-track-preview="https://p.scdn.co/mp3-preview/ab3d501c5ffbf560e94094f76cd36d874a26e941?cid=8897482848704f2a8f8d7c79726a70d4">
+            <p class="music-number">1</p>
+            <p class="music-title">Around The World</p>
+            <p class="music-duration">238733</p>
+        </div>`;
+
+	const markupTwo = `
+        <div class="music" data-track-preview="https://p.scdn.co/mp3-preview/ab3d501c5ffbf560e94094f76cd36d874a26e941?cid=8897482848704f2a8f8d7c79726a70d4">
+            <p class="music-number">1</p>
+            <p class="music-title">Around The World</p>
+            <p class="music-duration">238733</p>
+        </div>
+        <div class="music" data-track-preview="https://p.scdn.co/mp3-preview/ab3d501c5ffbf560e94094f76cd36d874a26e941?cid=8897482848704f2a8f8d7c79726a70d4">
+            <p class="music-number">1</p>
+            <p class="music-title">Around The World</p>
+            <p class="music-duration">238733</p>
+        </div>`;
+
+	it('should create and append the markup given a correct data', () => {
+		const element = document.createElement('div');
+		renderAlbumTracks(data, element);
+
+		expect(element.innerHTML).to.eql(markup);
+	});
+
+	it('should create and append the markup when more than 1 item', () => {
+		const elementTwo = document.createElement('div');
+		renderAlbumTracks(dataTwo, elementTwo);
+
+		expect(elementTwo.innerHTML).to.eql(markupTwo);
+	});
+});
