2ebe84c40da6e1e494599894aae2f320447a931d
Daniel Silva <dani3l.dxp@gmail.com>
2020-05-29 12:38:48 -0300

    feat: importando o searchTrigger ao main

--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
 'use strict';
 import spotify from './spotify';
-import renderAlbum from './albumList';
 import renderAlbumInfo from './albumInfo';
 import renderAlbumTracks from './albumTracks';
-const albumList = document.querySelector('#album-list');
+import searchEnterTrigger from './searchTrigger';
+
+searchEnterTrigger();
+
 const albumInfo = document.querySelector('#album-info');
 const albumTracks = document.querySelector('#album-tracks');
 
-const albums = spotify.search.albums('Zé Neto e Cristiano');
 const album = spotify.album.getAlbum('1Yg3QTRv9IGAWeJQb7q8bT');
 
-albums.then((data) => renderAlbum(data.albums.items, albumList));
 album
 	.then((data) => renderAlbumInfo(data, albumInfo))
 	.then((data) => renderAlbumTracks(data.tracks.items, albumTracks));
