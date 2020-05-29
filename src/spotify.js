ce90e9618e6168c93720042cc366c38829ac600e
Daniel Silva <dani3l.dxp@gmail.com>
2020-05-29 15:03:35 -0300

    feat: alterando token

--- src/spotify.js
+++ src/spotify.js
@@ -3,7 +3,7 @@ import SpotifyWrapper from 'dxp-spotify-api-wrapper';
 
 const spotify = new SpotifyWrapper({
 	apiURL: `https://api.spotify.com/v1`,
-	token: `BQCzqkpwKS01T3dU9OO8DtFpJDyaSFrAnA2GBdUP5lq4B-LfNUz2CkAMGDH-sI-1gxuNzjHy2YHPTHFiip6mbUPPmtDqRDmt5zr6Fd5gEgedflKRnH8lXwG2WMLMiMI12iLieWOLI0y53pI`
+	token: `BQCmPwBaIltjeHFN5LELRaFcNs3GyU3OMUe6XhzBFsjt25j4maBQEvuVeyxcF5_gJF3u2Dz0y-CGPJ7qG_bEsgL4yqGfntMMo4KeWbsE9VhECMDKpQrpSCMQJ-_uF9t71Zkztkdezt_FPtA`
 });
 
 export default spotify;
