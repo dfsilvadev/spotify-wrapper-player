5fe0002f88bda6d14a69ac88aff70bcb47d1cc30
Daniel Silva <dani3l.dxp@gmail.com>
2020-05-29 11:33:34 -0300

    feat: renderizando o componente no html

--- src/main.js
+++ src/main.js
@@ -2,12 +2,15 @@
 import spotify from './spotify';
 import renderAlbum from './albumList';
 import renderAlbumInfo from './albumInfo';
+import renderAlbumTracks from './albumTracks';
+const albumList = document.querySelector('#album-list');
+const albumInfo = document.querySelector('#album-info');
+const albumTracks = document.querySelector('#album-tracks');
 
 const albums = spotify.search.albums('Zé Neto e Cristiano');
-const albumList = document.querySelector('#album-list');
-
 const album = spotify.album.getAlbum('1Yg3QTRv9IGAWeJQb7q8bT');
-const albumInfo = document.querySelector('#album-info');
 
 albums.then((data) => renderAlbum(data.albums.items, albumList));
-album.then((data) => renderAlbumInfo(data, albumInfo));
+album
+	.then((data) => renderAlbumInfo(data, albumInfo))
+	.then((data) => renderAlbumTracks(data.tracks.items, albumTracks));
